8b93dcce29b79b070ea3251e3958578f437304d3
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-09-25 09:59:34 +0300

    add: get-ascii-value-of-character.js

---
+++ 8-kyu/get-ascii-value-of-character.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+    Get ASCII value of a character.
+
+    For the ASCII table you can refer to http://www.asciitable.com/
+*/
+
+const getASCII = (c) => c.charCodeAt()
+console.log(getASCII('A'))
+console.log(getASCII('5'))
