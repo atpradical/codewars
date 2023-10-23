72be8b5876dc38ffcad020787c27440bcd07dad1
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-10-23 21:51:17 +0300

    add: How many arguments

---
+++ 7-kyu/How many arguments.js
@@ -0,0 +1,12 @@
+/*
+Write a function that returns the number of arguments it received.
+    args_count() --> 0
+    args_count('a') --> 1
+    args_count('a', 'b') --> 2
+*/
+
+function args_count() {
+    return arguments.length
+}
+
+console.log(args_count(1,2,3))
